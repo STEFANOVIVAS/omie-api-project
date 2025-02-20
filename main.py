8ef1d10e619f50ec706105082aa60ff6a27a8e1a
STEFANOVIVAS <stefanovivas@gmail.com>
2025-02-20 12:27:59 -0300

    feature: Implementing manual schema evolution

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.config import Settings
 import requests
 import json
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 import pandas as pd
 import logging
 from requests.adapters import HTTPAdapter,Retry
@@ -45,7 +45,7 @@ def get_total_pages(resource:str,action:str,params:dict)->int:
         
     }
     response=request(resource,body)
-    total_of_pages=response.get("total_de_paginas",0)
+    total_of_pages=response.get("total_de_paginas",response.get("nTotPaginas"))
     return total_of_pages
 
 def save_file(resource:str,content:dict):
@@ -53,14 +53,43 @@ def save_file(resource:str,content:dict):
     content=json.dumps(content)
     with open(f"{file_name}.json","w") as file:
         file.write(content)
-def save_into_db(resource:str,content:dict,page:int):
-    db_name=resource.split("/")[-2]
+def get_engine():
     engine=create_engine(f"postgresql://{settings.DB_USERNAME}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
-    df=pd.json_normalize(content)
+    return engine
+def alter_table_schema(table_name:str,df:pd.DataFrame):
+    engine=get_engine()
+    exist_columns=get_columns_db(table_name)
+    new_columns=[column for column in df.columns if column not in exist_columns]
+    connection=engine.connect()
+    connection.begin()
+    for column in new_columns:
+        query=text(f"""ALTER TABLE {table_name} ADD COLUMN "{column}" TEXT""")
+        connection.execute(query)
+        print(f"Column {column} added to table {table_name}")
+    connection.commit()
+    connection.close()
+
+def get_columns_db(table_name:str):
+    engine=get_engine()
+    connection=engine.connect()
+    query=text(f"""SELECT column_name
+    FROM information_schema.columns
+    WHERE table_name = '{table_name}'""")
+    result=connection.execute(query)
+    columns=[column[0] for column in result]
+    return columns
+
+def save_into_db(resource:str,content:dict,page:int):
+    table_name=resource.split("/")[-2]
+    df=pd.json_normalize(content,sep="_")
+    engine=get_engine()
+    
+    
     if page==1:
-        df.to_sql(db_name,engine,if_exists="replace",index=False)
+        df.to_sql(table_name,engine,if_exists="replace",index=False)
     else:
-        df.to_sql(db_name,engine,if_exists="append",index=False)
+        alter_table_schema(table_name,df)
+        df.to_sql(table_name,engine,if_exists="append",index=False)
     
 def custom_session():    
     session = requests.Session()
@@ -82,7 +111,10 @@ for endpoint in endpoints:
     records_fetched=0
     # for page in range(1,total_of_pages+1):
     for page in range(1,4):
-        params["pagina"]=page
+        if params.get("pagina") is not None:
+            params["pagina"]=page
+        if params.get("nPagina") is not None:
+            params["nPagina"]=page
         body={
             "call":action,
             "app_key": app_key,
@@ -91,11 +123,14 @@ for endpoint in endpoints:
 
         
         response=request(resource,body)
-        records_fetched+=response.get("registros",0)
+        print(response)
+        records_fetched+=response.get("registros",response.get("nRegistros"))
 
         contents=response.get(chave,[])
-        black_list=["tags","recomendacoes","homepage","fax_ddd","bloquear_exclusao","produtor_rural","enderecoEntrega"]
-        # lista=[key for key in content.keys() if key not in black_list]
+        print(contents)
+        # black_list=["tags","recomendacoes","homepage","fax_ddd","bloquear_exclusao","produtor_rural","enderecoEntrega"]
+        black_list=["tags"]
+        # # lista=[key for key in content.keys() if key not in black_list]
         lista = [{key: value for key, value in content.items() if key not in black_list} for content in contents]
         # lista=[content for content in contents if content.keys() not in black_list]
         # for content in contents:
